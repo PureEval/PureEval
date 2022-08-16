9a664f591b556d6b53b9946a108b033e64d78265
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-16 23:41:53 +0800

    feat: reject either both

--- PureEval.js
+++ PureEval.js
@@ -1,14 +1,14 @@
 import { curry,curry_any,uncurry } from './src/curry.js'
-import { filter,shied,choose } from './src/filter.js'
+import { filter,reject,shied,choose } from './src/filter.js'
 import { iterate,map,foreach,reduce,pipe } from './src/iterate.js'
-import { add,minus,mul,div,mod,rema,power,sum,negate,average,median } from './src/math.js'
+import { add,minus,mul,div,mod,rema,power,either,both,sum,negate,average,median } from './src/math.js'
 import { range } from './src/range.js'
 import { compose } from './src/transform.js'
 export {
     curry,curry_any,uncurry,
-    filter,shied,choose,
+    filter,reject,shied,choose,
     iterate,map,foreach,reduce,pipe,
-    add,minus,mul,div,mod,rema,power,sum,negate,average,median,
+    add,minus,mul,div,mod,rema,power,either,both,sum,negate,average,median,
     range,
     compose
 }
