f5b867207d4b786da87762bcc0dc6c24e86ce71b
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-17 00:20:20 +0800

    + update functions and fix bugs
    - 增加比较函数
    - 增加排序器和sort函数
    - 修复curry_any因为参数为数组导致的长度改变问题

--- PureEval.js
+++ PureEval.js
@@ -1,14 +1,14 @@
 import { curry,curry_any,uncurry } from './src/curry.js'
 import { filter,reject,shied,choose } from './src/filter.js'
 import { iterate,map,foreach,reduce,pipe } from './src/iterate.js'
-import { add,minus,mul,div,mod,rema,power,either,both,sum,negate,average,median } from './src/math.js'
+import { add,minus,mul,div,mod,rema,power,either,both,gt,gte,lt,lte,upper,under,sort,sum,negate,average,median } from './src/math.js'
 import { range } from './src/range.js'
 import { compose } from './src/transform.js'
 export {
     curry,curry_any,uncurry,
     filter,reject,shied,choose,
     iterate,map,foreach,reduce,pipe,
-    add,minus,mul,div,mod,rema,power,either,both,sum,negate,average,median,
+    add,minus,mul,div,mod,rema,power,either,both,gt,gte,lt,lte,upper,under,sort,sum,negate,average,median,
     range,
     compose
 }
