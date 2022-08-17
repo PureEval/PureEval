e839b7e8fc1051c5ac5ba1e80161e8f2c3065d4a
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-17 10:38:56 +0800

    doc: introducing jsdoc for the project

--- src/transform.js
+++ src/transform.js
@@ -1,5 +1,5 @@
 function compose(...fun){
     if(fun.length===1)return fun[0];
-    return fun.reduce((a, b)=>(...args)=>a(b(...args)))
+    return fun.reduce((a,b)=>(...args)=>a(b(...args)))
 }
 export { compose }
