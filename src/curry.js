a77f52b3d7937809f3243b5fe9f0e01e11304a5b
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-19 20:21:05 +0800

    refactor: curry/transform/iterator
    - 修复 iterator 中因为数组浅复制产生的迭代问题
    - 更改了一种 curry 实现方式
    - 将 pipe 迁移到 transform 中

--- src/curry.js
+++ src/curry.js
@@ -1,23 +1,6 @@
 function curry(fun,save=true){
-    let args="",i=0,s=fun.toString(),result,flag=0;
-    if(s.substring(0,8)=='function'){
-        while(s[i]!='(')++i;
-        for(++i;s[i]!='{';++i)args+=s[i];
-    }else{
-        while(!(('a'<=s[i]&&s[i]<='z')||('A'<=s[i]&&s[i]<='Z')))++i;
-        for(;s[i]!='>';++i)args+=s[i];
-        while(++i){
-            if(s[i]=='{')break;
-            if(s[i]!='{'&&s[i]!=' '){
-                flag=1;
-                break;
-            }
-        }
-    }
-    args=args.replace(/[^_^,|^\[a-zA-Z0-9\]]/g,'').split(',');
-    if(flag==0)result=new Function(args[args.length-1],`arguments=Object.assign({},[${args.toString()}]);`+s.substring(i+1,s.length-1));
-    else result=new Function(args[args.length-1],`arguments=Object.assign({},[${args.toString()}]);return ${s.substring(i,s.length)}`);
-    for(let j=args.length-2;j>=0;--j)result=new Function(args[j],`return ${result.toString()}`);
+    const summon=(args,rest)=>(rest?arg=>summon([...args, arg],rest-1):fun(...args));
+    const result=summon([],fun.length);
     if(save)result.origin=fun;
     return result;
 }
@@ -41,7 +24,7 @@ function curry_any(fun,save=true){
     args.forEach(v=>head+=`${v}=${v}[0];`);
     if(flag==0)result=new Function(`...${args[args.length-1]}`,head+`arguments=Object.assign({},[${args.toString()}]);`+s.substring(i+1,s.length-1));
     else result=new Function(`...${args[args.length-1]}`,`${head}arguments=Object.assign({},[${args.toString()}]);return ${s.substring(i,s.length)}`);
-    for(let j=args.length-2;j>=0;--j)result=new Function(`...${args[j]}`,`let _u=${result.toString()},i,_t=${args[j]};if(_t.length>1)for(i=1;i<_t.length;++i)_u=_u(_t[i]);return _u;`);
+    for(let j=args.length-2;j>=0;--j)result=new Function(`...${args[j]}`,`let _u=${result.toString()},_t=${args[j]};if(_t.length>1)for(let i=1;i<_t.length;++i)_u=_u(_t[i]);return _u;`);
     if(save)result.origin=fun;
     return result;
 }
