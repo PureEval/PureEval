fb1ef7f0b052fb141745949477a70225a6592a48
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-19 20:21:05 +0800

    = curry transform iterator
    - 修复 iterator 中因为数组浅复制产生的迭代问题
    - 更改了一种 curry 实现方式
    - 将 pipe 迁移到 transform 中

--- PureEval.js
+++ PureEval.js
@@ -1,16 +1,16 @@
 import { curry,curry_any,uncurry } from './src/curry.js'
 import { filter,reject,shied,choose } from './src/filter.js'
-import { iterate,map,foreach,reduce,pipe } from './src/iterate.js'
+import { iterate,map,foreach,reduce } from './src/iterate.js'
 import { add,minus,mul,div,mod,rema,power,either,both,gt,gte,lt,lte,upper,under,sort,sum,negate,average,median } from './src/math.js'
 import { range } from './src/range.js'
-import { compose } from './src/transform.js'
+import { compose,pipe } from './src/transform.js'
 import { place,bind } from './src/bind.js'
 export {
     curry,curry_any,uncurry,
     filter,reject,shied,choose,
-    iterate,map,foreach,reduce,pipe,
+    iterate,map,foreach,reduce,
     add,minus,mul,div,mod,rema,power,either,both,gt,gte,lt,lte,upper,under,sort,sum,negate,average,median,
     range,
-    compose,
+    compose,pipe,
     place,bind
 }
