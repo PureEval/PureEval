3ae414a2b422affb1019cb6e09335f97cfa8f008
东灯 <43312495+Lampese@users.noreply.github.com>
2022-08-27 15:08:22 +0800

    + ifElse

--- src/logic.js
+++ src/logic.js
@@ -8,12 +8,34 @@ const lte=curry_any((a,b)=>a<=b);
 const equal=curry_any((a,b)=>a==b);
 const when=curry_any((a,b)=>{
     return function(obj){
-        if(a(obj))return b(obj);
+        if(a(obj))return b();
     }
 });
 const unless=curry_any((a,b)=>{
     return function(obj){
+        if(!a(obj))return b();
+    }
+});
+const ifElse=curry_any((a,b,c)=>{
+    return function(obj){
+        if(a(obj))return b();
+        else return c();
+    }
+});
+const when_v=curry_any((a,b)=>{
+    return function(obj){
+        if(a(obj))return b(obj);
+    }
+});
+const unless_v=curry_any((a,b)=>{
+    return function(obj){
         if(!a(obj))return b(obj);
     }
 });
-export { either,both,gt,gte,lt,lte,equal,when,unless }
+const ifElse_v=curry_any((a,b,c)=>{
+    return function(obj){
+        if(a(obj))return b(obj);
+        else return c(obj);
+    }
+});
+export { either,both,gt,gte,lt,lte,equal,when,unless,ifElse,when_v,unless_v,ifElse_v }
