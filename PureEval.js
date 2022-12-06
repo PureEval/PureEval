a8bc17b4050823116e067304da8e8d1990a8740b
东灯 <43312495+Lampese@users.noreply.github.com>
2022-12-06 10:51:36 +0800

    index: add recent features

--- PureEval.js
+++ PureEval.js
@@ -8,17 +8,15 @@ import { _,bind } from './src/bind.js'
 import { either,both,gt,gte,lt,lte,equal,when,unless,ifElse,when_v,unless_v,ifElse_v } from './src/logic.js'
 import { summon } from './src/summon.js'
 import { match } from './src/match.js'
-import { take,elem,zip } from './src/list.js'
 export {
     curry,curry_any,uncurry,
     filter,reject,shied,choose,
     iterate,map,flatMap,foreach,reduce,fold,scan,
-    add,minus,mul,div,mod,rema,power,upper,under,sort,sum,negate,average,median,max,min,
+    odd,even,add,minus,mul,div,mod,rema,power,upper,under,sort,sum,negate,average,median,max,min,
     range,
     compose,pipe,call,
     _,bind,
     either,both,gt,gte,lt,lte,equal,when,unless,ifElse,when_v,unless_v,ifElse_v,
     summon,
-    match,
-    take,elem,zip
+    match
 }
