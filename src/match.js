df6495bd3dd2be09053416b5a3d88bd6d27e3793
东灯 <43312495+Lampese@users.noreply.github.com>
2022-12-18 10:12:05 +0800

    feat: arr_equal common always
    - 添加三个功能性函数
    - match支持匹配数组
    - 更改 iterate 中函数的参数顺序，使其更合理

--- src/match.js
+++ src/match.js
@@ -1,9 +1,13 @@
 import { _ } from './bind.js' 
+import { arr_equal } from './logic.js';
 function match(...rules){
     return function(value){
-        for(let i=0;i<rules.length;i+=2)
+        for(let i=0;i<rules.length;i+=2){
             if(value==rules[i]||rules[i]==_)
                 return rules[i+1](value);
+            else if(Array.isArray(rules[i])&&Array.isArray(value)&&arr_equal(value,rules[i]))
+                return rules[i+1](value);
+        }
     }
 }
 export { match };
