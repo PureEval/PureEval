fdd2349bd54d77d01ca1c14973ad0046c13857fd
Lampese <me@Lampese.com>
2022-12-30 00:23:50 +0800

    doc: remove jsdoc
    Bye Jsdoc!

--- docs/src/summon.js
+++
@@ -1,6 +0,0 @@
-function __summon(total,fn){
-    let result;
-    eval(`result=function(${Array(total).fill(0).map((v,i)=>"a"+i).toString()}){return fn.apply(this,arguments)}`);
-    return result;
-}
-export { __summon };
