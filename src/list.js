7dca2e54d1aecac77afa002eeac06114b1d9bc42
东灯 <me@lampese.com>
2023-04-29 22:24:21 +0800

    revert: remove when_v/ifElse_v/unless_v

--- src/list.js
+++ src/list.js
@@ -10,10 +10,15 @@ const zipWith = curry((f, a, b) => {
 	}
 	return result;
 });
+
 const zip = zipWith((a, b) => [a, b]);
+
 const join = curry((s, arr) => arr.join(s));
+
 const slice = curry((start, end, arr) => arr.splice(start, end));
+
 const take = curry((pos, arr) => slice(0, pos - 1, arr));
+
 const takeWhile = curry((f, arr) => {
 	const result = [],
 		end = arr.length;
@@ -25,7 +30,9 @@ const takeWhile = curry((f, arr) => {
 	}
 	return result;
 });
+
 const drop = curry((pos, arr) => arr.slice(pos));
+
 const dropWhile = curry((f, arr) => {
 	const result = [],
 		end = arr.length;
@@ -41,6 +48,7 @@ const dropWhile = curry((f, arr) => {
 	}
 	return result;
 });
+
 const allCheck = curry((f, arr) => {
 	let index = 0;
 	const end = arr.length;
@@ -50,6 +58,7 @@ const allCheck = curry((f, arr) => {
 	}
 	return true;
 });
+
 const anyCheck = curry((f, arr) => {
 	let index = 0;
 	const end = arr.length;
@@ -59,15 +68,22 @@ const anyCheck = curry((f, arr) => {
 	}
 	return false;
 });
+
 const concat = curry((a, b) => {
 	if (Array.isArray(a)) return a.concat(b);
 	else return a + b;
 });
+
 const head = (arr) => arr[0];
+
 const tail = (arr) => arr[arr.length - 1];
+
 const dropHead = (arr) => drop(1, arr);
+
 const dropTail = (arr) => arr.splice(0, -1);
+
 const includes = curry((a, b) => a.includes(b));
+
 function reverse(arr) {
 	const result = [];
 	let end = arr.length - 1;
