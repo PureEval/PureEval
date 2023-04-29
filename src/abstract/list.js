6ff8febb3a6297880acfa39ae9ab49fdfecffa3f
东灯 <me@lampese.com>
2023-04-29 19:07:43 +0800

    revert: remove zip(List(Cons))
    - 暂时删除 cons list 的  zip 方法

--- src/abstract/list.js
+++ src/abstract/list.js
@@ -154,12 +154,9 @@ const zipWith = curry(
 );
 
 //Unverified
-const zip = zipWith((x, y) => [x, y]);
-
-//Unverified
 const shied = curry((v, xs) => reject(includes(v), xs));
 
 //Unverified
 const choose = curry((v, xs) => filter(includes(v), xs));
 
-export { seq, head, isEmpty, range, lazy, tail, iterate, map, faltMap, zip, choose, dropWhile, take, takeWhile, drop, filter, reject, repeat, shied, forEach, concat, includes };
+export { seq, head, isEmpty, range, lazy, tail, iterate, map, faltMap, choose, dropWhile, take, takeWhile, drop, filter, reject, repeat, shied, forEach, concat, includes };
