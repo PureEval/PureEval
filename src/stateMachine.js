6a09df2b5defc424d4d0ab3b11c0e24750ef1ada
东灯 <me@lampese.com>
2023-04-30 18:26:23 +0800

    = refactor
    - 重构了大量函数的实现

--- src/stateMachine.js
+++ src/stateMachine.js
@@ -7,43 +7,55 @@ import { compose, pipe } from './transform.js';
 const higherPipe = curry((functions, iv) => {
 	const processed = [],
 		processedIv = [];
-	let firstFunction, firstFunctionIv;
 	iv.forEach((value, index) => {
 		if (value !== 0) {
 			processed.push(functions[index]);
 			processedIv.push(value);
 		}
 	});
-	(firstFunction = processed.shift()), (firstFunctionIv = processedIv.shift());
+	const firstFunction = processed.shift(),
+		firstFunctionIv = processedIv.shift();
 	if (processed.length === 0) return fold(firstFunction, firstFunctionIv);
-	else
-		return summon(firstFunction.length, (...args) =>
-			pipe.apply(
-				this,
-				processed.map((value, index) => fold(value, processedIv[index]))
-			)(firstFunction.length === 1 ? fold(firstFunction, firstFunctionIv, args[0]) : firstFunction(...args))
+	else {
+		const construct = pipe.apply(
+			this,
+			processed.map((value, index) => fold(value, processedIv[index]))
 		);
+		return summon(firstFunction.length, (...args) =>
+			construct(
+				firstFunction.length === 1
+					? fold(firstFunction, firstFunctionIv, args[0])
+					: firstFunction(...args)
+			)
+		);
+	}
 });
 
 const higherComp = curry((functions, iv) => {
 	const processed = [],
 		processedIv = [];
-	let firstFunction, firstFunctionIv;
 	iv.forEach((value, index) => {
 		if (value !== 0) {
 			processed.push(functions[index]);
 			processedIv.push(value);
 		}
 	});
-	(firstFunction = processed.pop()), (firstFunctionIv = processedIv.pop());
+	const firstFunction = processed.pop(),
+		firstFunctionIv = processedIv.pop();
 	if (processed.length === 0) return fold(firstFunction, firstFunctionIv);
-	else
-		return summon(firstFunction.length, (...args) =>
-			compose.apply(
-				this,
-				processed.map((value, index) => fold(value, processedIv[index]))
-			)(firstFunction.length === 1 ? fold(firstFunction, firstFunctionIv, args[0]) : firstFunction(...args))
+	else {
+		const construct = compose.apply(
+			this,
+			processed.map((value, index) => fold(value, processedIv[index]))
 		);
+		return summon(firstFunction.length, (...args) =>
+			construct(
+				firstFunction.length === 1
+					? fold(firstFunction, firstFunctionIv, args[0])
+					: firstFunction(...args)
+			)
+		);
+	}
 });
 
 const coalgebra = curry((seed, next) => () => (seed = next(seed)));
