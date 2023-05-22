2b9aafe763c701ba83388b71cec1cea86c0e1d74
东灯 <me@lampese.com>
2023-05-22 23:08:38 +0800

    = bind

--- test/test.js
+++ test/test.js
@@ -108,6 +108,10 @@ function Bind() {
 				const foo = (a, b, c) => a + b + c;
 				assert.equal(bind(foo, 1, _, 3)(4), 8);
 			});
+            it('Arguments', () => {
+				const foo = (a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12) => a8;
+				assert.equal(bind(foo, 1, _, 3)(2,4,5,6,7,8,9), 8);
+			});
 		});
 	});
 }
