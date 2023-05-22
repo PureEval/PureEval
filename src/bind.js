585ac9e3fe69247565e99da4b48caf8eb03a158b
东灯 <me@lampese.com>
2023-05-22 23:08:38 +0800

    fix: bind cannot handle trailing parameters

--- src/bind.js
+++ src/bind.js
@@ -2,15 +2,10 @@ import { summon } from './summon.js';
 
 const _ = { _P: 1 };
 const bind = (func, ...args) => {
-	const pos = args.reduce((acc, curr, index) => {
-		if (curr === _) acc.push(index);
-		return acc;
-	}, []);
-	return summon(pos.length, (...a) => {
-		const arg = [...args];
-		pos.forEach((v) => (arg[v] = a.shift()));
-		return func(...arg);
-	});
+	if (args.length < func.length)
+		args = args.concat(new Array(func.length - args.length + 1).fill(_));
+	const pos = args.reduce((acc, curr, index) => (curr === _ ? [...acc, index] : acc), []);
+	return summon(pos.length, (...a) => func(...args.map((v) => (v === _ ? a.shift() : v))));
 };
 
 export { _, bind };
