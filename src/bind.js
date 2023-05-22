d28f9c63c10dd752560a9fb0a2b158b00ed41d08
xBoyMinemc <86919167+xBoyMinemc@users.noreply.github.com>
2023-05-22 22:52:34 +0800

    Update bind.js

--- src/bind.js
+++ src/bind.js
@@ -1,15 +1,17 @@
 import { summon } from './summon.js';
 
 const _ = { _P: 1 };
+
 const bind = (func, ...args) => {
 	const pos = args.reduce((acc, curr, index) => {
 		if (curr === _) acc.push(index);
 		return acc;
 	}, []);
-	return summon(pos.length, (...a) => {
-		const arg = [...args];
-		pos.forEach((v) => (arg[v] = a.shift()));
-		return func(...arg);
+	return summon(func.length - args.length + pos.length, (...a) => {
+		const argsFinal = Array.from({ length: func.length }, (v, i) => args[i] || _);
+		let j = 0;
+		argsFinal.forEach((v, i) => (v === _ ? (argsFinal[i] = a[j++]) : 0));
+		return func(...argsFinal);
 	});
 };
 
