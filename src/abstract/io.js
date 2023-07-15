22667aad705c115f937804b588eede781839ffcb
东灯 <me@lampese.com>
2023-07-15 18:29:38 +0800

    feat: Algebraic Effects

--- src/abstract/io.js
+++ src/abstract/io.js
@@ -1,7 +1,12 @@
-const IO = (effect) => ({
-	map: (fn) => IO(() => fn(effect())),
-	chain: (fn) => fn(effect()),
-	run: () => effect()
-});
+const IO = (effect) => {
+	let perform;
+	const obj = {
+		map: (fn) => IO(() => fn(effect(perform))),
+		chain: (fn) => fn(effect(perform)),
+		handle: (fn) => ((perform = fn), obj),
+		run: () => effect(perform)
+	};
+	return obj;
+};
 
 export { IO };
--- src/abstract/ioasync.js
+++ src/abstract/ioasync.js
@@ -1,7 +1,12 @@
-const IOAsync = (effect) => ({
-	map: (fn) => IOAsync(async () => await fn(await effect())),
-	chain: (fn) => IOAsync(async () => await fn(await effect()).run()),
-	run: async () => await effect()
-});
+const IOAsync = (effect) => {
+	let perform;
+	const obj = {
+		map: (fn) => IOAsync(async () => await fn(await effect(perform))),
+		chain: (fn) => IOAsync(async () => await fn(await effect(perform)).run()),
+		handle: (fn) => ((perform = fn), obj),
+		run: async () => await effect(perform)
+	};
+	return obj;
+};
 
 export { IOAsync };
