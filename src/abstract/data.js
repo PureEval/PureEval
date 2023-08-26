816a0652bdcef07b5ecc376bfc5fe371f216c743
东灯 <me@lampese.com>
2023-08-26 22:09:43 +0800

    feat: Data provides binder for easy binding

--- src/abstract/data.js
+++ src/abstract/data.js
@@ -9,6 +9,7 @@ const Data = (...args) => {
 	let show = (D) => D.type;
 
 	const data = {
+		binder: {},
 		from: (v) => v instanceof DATA,
 		bindShow: (f) => (show = f)
 	};
@@ -17,28 +18,23 @@ const Data = (...args) => {
 		const functions = value.trim().split(' '),
 			functionName = functions.shift();
 		if (functions.length) {
-			data[functionName] = summon(
-				functions.length,
-				(...iargs) => {
-					const result = new DATA(functionName);
-					functions.forEach(
-						(item, idx) => (result[item] = iargs[idx])
-					);
-					result.args = iargs;
-					result.show = function () {
-						return show(this);
-					};
-					return result;
-				}
-			);
+			data[functionName] = summon(functions.length, (...iargs) => {
+				const result = new DATA(functionName);
+				functions.forEach((item, idx) => (result[item] = iargs[idx]));
+				result.args = iargs;
+				result.show = function () {
+					return show(this);
+				};
+				return result;
+			});
 		} else {
 			data[functionName] = new DATA(functionName);
 			data[functionName]['show'] = function () {
 				return show(this);
 			};
 		}
-		data[functionName]['is'] = (val) =>
-			val instanceof DATA && val.type === functionName;
+		data[functionName]['is'] = (val) => val instanceof DATA && val.type === functionName;
+		data.binder[functionName] = data[functionName];
 	});
 
 	return data;
